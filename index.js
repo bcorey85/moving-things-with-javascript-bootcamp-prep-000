f69368ee199c4633e7281ae1544925cd1654a996
brandon corey <bcorey85@gmail.com>
2019-03-31 19:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
+dodger.style.backgroundColor = '#FF69B4'
+
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
